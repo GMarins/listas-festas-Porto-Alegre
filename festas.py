ea548c502e6fb1eaf0d3a76d4f51d368405d7f8f
Gabriel Marins <gmc.marins@gmail.com>
2016-07-29 23:25:48 -0300

    Update festas.py

--- festas.py
+++ festas.py
@@ -3,7 +3,8 @@
 ### PARTY LIST  v0.1a
 
 
-#Esse arquivo contém as funções especiais
+#Esse arquivo contém as funções utilizadas
+#As listas com os nomes são separadas devido às restrições de limite no número de nomes inscritos de cada casa
 
 import re
 import mechanize
@@ -11,7 +12,7 @@ import bs4
 
 def cucko(s): 
 	br = mechanize.Browser()
-	nomes = ['Name1','Name2'] #Insert the names
+	nomes = ['Name1','Name2'] #Insert the names, unlimited amount
 	email = '' #Insert your email
 
 	for nome in nomes:
@@ -27,7 +28,7 @@ def sinners(s): #Fully Functional, mult names, one event per time CHECK
 
 	retorno = br.open(s)
 
-	nomes = ['Name1','Name2'] #Insert the names
+	nomes = ['Name1','Name2'] #Insert the names, max 5
 	email = '' #Insert your email
 
 	for f in br.forms():
@@ -47,7 +48,7 @@ def sinners(s): #Fully Functional, mult names, one event per time CHECK
 def margot(s): #Seems working
 	"""61"""
 	br = mechanize.Browser()
-	nomes = ['Name1','Name2'] #Insert the names
+	nomes = ['Name1','Name2'] #Insert the names, max 5
 	email = '' #Insert your email
 
 	retorno = br.open(s)
@@ -69,7 +70,7 @@ def beco(s):
 
 	br = mechanize.Browser()
 
-	nomes = ['Name1','Name2'] #Insert the names
+	nomes = ['Name1','Name2'] #Insert the names, max 10
 	email = '' #Insert your email
 
 
